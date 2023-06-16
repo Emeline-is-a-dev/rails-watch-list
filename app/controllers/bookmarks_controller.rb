e1f02799dd71727947f7a83d9a8bd97aa6dc9d6b
emeline fabiani <emeline.fabiani@kedgebs.com>
2023-06-16 14:20:18 +0200

    exo_watch_list

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,38 @@
+class BookmarksController < ApplicationController
+  def index
+    @list = List.find(params[:list_id])
+    @bookmarks = @list.bookmark
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:list_id])
+  end
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(params_bookmark)
+    @bookmark.list = @list
+    if @bookmark.save!
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark), status: :see_other
+  end
+
+  private
+
+  def params_bookmark
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  # get 'bookmarks/index'
+  # get 'bookmarks/new'
+  # get 'lists/index'
+  # get 'lists/show'
+  # get 'lists/new'
+  resources :lists, only: [:new, :index, :show, :create] do
+    resources :bookmarks, only: [:index, :new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
